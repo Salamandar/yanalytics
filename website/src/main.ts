37f027f8fb43c9da30bc4540bdc32a0c0619da77
Félix Piédallu <felix@piedallu.me>
2026-07-10 21:46:47 +0200

    website: run format

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import "@fontsource-variable/source-sans-3/wght.css"
+import '@fontsource-variable/source-sans-3/wght.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
