b6fea0889f00a7a53d7b346bea9c17faad95a0eb
Félix Piédallu <felix@piedallu.me>
2026-07-14 17:33:57 +0200

    website: Add PageNotFound

---
+++ website/src/components/PageNotFound.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="centered">
+  <span>
+  This page was not found!
+</span>
+</div>
+
+</template>
+
+<style scoped>
+.centered {
+  margin-top: 3em;
+  text-align: center;
+}
+
+</style>
--- website/src/main.ts
+++ website/src/main.ts
@@ -7,12 +7,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
 import AnalyticsView from './components/AnalyticsView.vue'
+import PageNotFound from './components/PageNotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-     { path: '/', component: AnalyticsView},
-  ]
+    { path: '/', component: AnalyticsView },
+    { path: '/:pathMatch(.*)*', component: PageNotFound },
+  ],
 })
 
 const app = createApp(App)
