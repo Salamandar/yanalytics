d61fd1cd3fd5e69ce4c29b9275aad0d0e33ab453
Félix Piédallu <felix@piedallu.me>
2026-07-14 17:21:33 +0200

    website: main: Use vue-router

--- website/src/main.ts
+++ website/src/main.ts
@@ -3,6 +3,18 @@ import './assets/main.css'
 import '@fontsource-variable/source-sans-3/wght.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import AnalyticsView from './components/AnalyticsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+     { path: '/', component: AnalyticsView},
+  ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
