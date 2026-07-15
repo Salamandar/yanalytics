66a29fdbf384ae51fc82d1955925c439f3638425
Félix Piédallu <felix@piedallu.me>
2026-07-16 00:08:24 +0200

    website: Add navbar

--- website/src/components/MainHeader.vue
+++ website/src/components/MainHeader.vue
@@ -3,14 +3,25 @@ import ynhLogo from '../assets/ynh_logo_white.svg'
 </script>
 
 <template>
-  <span class="site-title">
-    <img :src="ynhLogo" alt="YunoHost Logo" />
-    YunoHost Analytics
-  </span>
+  <header class="header-bar">
+    <span class="site-title">
+      <img :src="ynhLogo" alt="YunoHost Logo" />
+      YunoHost Analytics
+    </span>
+    <nav class="site-nav">
+      <ul>
+        <li><router-link to="/">Installations</router-link></li>
+        <li><router-link to="/apps">Applications</router-link></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
 header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin-bottom: 2rem;
 }
 
@@ -30,5 +41,24 @@ header {
   .site-title > img {
     filter: invert(1);
   }
+}
+
+.site-nav ul {
+  list-style: none;
+  display: flex;
+}
+
+.site-nav li {
+  padding: 4px;
+}
+
+.site-nav li a {
+  padding: 0 4px;
+  text-decoration: none;
+  color: var(--color-text);
+}
+
+.router-link-exact-active {
+  border-bottom: 2px solid var(--ynh-blue);
 }
 </style>
