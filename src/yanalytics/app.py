3bd9501af33719685f83cb60dc0c69c9e844a2db
Félix Piédallu <felix@piedallu.me>
2026-07-16 22:08:25 +0200

    app: Return dummy data

--- src/yanalytics/app.py
+++ src/yanalytics/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import datetime
 import logging
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
@@ -90,6 +91,49 @@ def create_app(config: Config) -> FastAPI:
     @app.get("/api/v1/analytics/all")
     async def analytics() -> AnalyticsAggregate:
         return await compute_analytics_data()
+
+    @app.get("/api/v1/analytics/stats")
+    async def analytics_stats():
+        return {
+            "instances": [
+                {"year": 2010, "v12": 40, "v13": 10},
+                {"year": 2011, "v12": 30, "v13": 15},
+                {"year": 2012, "v12": 20, "v13": 20},
+                {"year": 2013, "v12": 15, "v13": 25},
+                {"year": 2014, "v12": 10, "v13": 30},
+                {"year": 2015, "v12": 8, "v13": 35},
+                {"year": 2016, "v12": 6, "v13": 40},
+            ],
+            "apps_nb": [
+                {"year": 2010, "count": 100},
+                {"year": 2011, "count": 105},
+                {"year": 2012, "count": 200},
+                {"year": 2013, "count": 205},
+                {"year": 2014, "count": 300},
+                {"year": 2015, "count": 305},
+                {"year": 2016, "count": 400},
+            ],
+        }
+
+    @app.get("/api/v1/analytics/apps")
+    async def analytics_apps():
+        apps = [
+            {"id": "nextcloud", "name": "Nextcloud", "count": 2367, "percent": 90},
+            {"id": "vaultwarden", "name": "Vaultwarden", "count": 1987, "percent": 70},
+            {"id": "my_webapp", "name": "My Webapp", "count": 1000, "percent": 40},
+        ] * 100
+
+        def lundi(i: int) -> str:
+            start = datetime.date(2018, 1, 1)
+            delta = datetime.timedelta(days=7 * i)
+            return (start + delta).strftime("%Y-%m-%d")
+
+        count_history = {lundi(i): i + 100 for i in range(440)}
+
+        return {
+            "details": apps,
+            "count_history": count_history,
+        }
 
     if config.testing:
 
